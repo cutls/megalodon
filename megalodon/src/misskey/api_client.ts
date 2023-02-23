bd0c017c009254f3e3f32454b066114495554a4e
cutls <web-pro@cutls.com>
2023-02-23 14:40:22 +0900

    quote must be null or status

--- megalodon/src/misskey/api_client.ts
+++ megalodon/src/misskey/api_client.ts
@@ -276,7 +276,7 @@ namespace MisskeyAPI {
         pinned: null,
         emoji_reactions: mapReactions(n.reactions, n.myReaction),
         bookmarked: false,
-        quote: n.renote !== undefined && n.text !== null
+        quote: n.renote && n.text ? note(n.renote, host) : null
       }
     }
 
